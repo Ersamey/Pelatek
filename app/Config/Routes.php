7cc7f990679fa9415da336316bc60eb10b5f657c
Ersamey <meiliaersa@gmail.com>
2024-06-21 19:00:44 +0700

    hapus routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -9,9 +9,6 @@ $routes->get('/', 'Home::index');
 $routes->get('/daftar', 'RegisterPlatek::index');
 $routes->post('daftar/save', 'RegisterPlatek::save');
 
-$routes->get('/loginn', 'Login::index');
-
-
 $routes->get('/admin', 'Admin::index', ['filter' => 'login']);
 $routes->get('/admin/acc/(:segment)', 'Admin::accPembayaran/$1', ['filter' => 'login']);
 $routes->get('/admin/pending/(:segment)', 'Admin::pendingPembayaran/$1', ['filter' => 'login']);
